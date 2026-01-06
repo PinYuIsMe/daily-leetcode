a9c8c08c92b26f6f02cd1a2b124e86b63e63f1b6
PinYuIsMe <93711490+PinYuIsMe@users.noreply.github.com>
2026-01-05 16:11:32 -0800

    Time: 16 ms (23.06%), Space: 18 MB (37.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,13 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) == len(t):
-            s = sorted(s)
-            t = sorted(t)
-            print(f"s: {s}")
-            print(f"t: {t}")
-            for i in range(len(s)):
-                if s[i] != t[i]:
-                    return False
-            return True
-        else:
-            return False
+        return sorted(s) == sorted(t)
