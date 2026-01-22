d0c0a250b5199d71de612691de6fa2f0349b76f1
PinYuIsMe <93711490+PinYuIsMe@users.noreply.github.com>
2026-01-21 17:33:54 -0800

    Time: 15 ms (78.48%), Space: 19.8 MB (10.15%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,18 +1,7 @@
-from collections import defaultdict
+from collections import Counter
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        magazineHash = defaultdict(int)
-        for letter in magazine:
-            if letter in magazineHash:
-                magazineHash[letter] += 1
-            else:
-                magazineHash[letter] = 1
-
-        for letter in ransomNote:
-            if letter in magazineHash:
-                magazineHash[letter] -= 1
-                if magazineHash[letter] == 0:
-                    del magazineHash[letter]
-            else:
-                return False
-        return True
+        # Counter creates a frequency map automatically
+        # Subtracting them keeps only the counts that are still needed
+        return not (Counter(ransomNote) - Counter(magazine))
